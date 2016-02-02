8c1fc10f3ac1cec7fc7439efce76e7d4d0cfb8b1
sGeeK <francois_perin@hotmail.fr>
2016-02-02 17:23:11 +0100

    Create new project for Contest.Business.GameStep

---
+++ Ihm/Contest.Business.GameStep/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest.Business.GameStep
+{
+    public class Class1
+    {
+    }
+}
