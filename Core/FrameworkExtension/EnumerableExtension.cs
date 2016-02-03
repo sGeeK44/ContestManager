6445e9f505b193d4681cbb43ce23713ef4e28421
sGeeK <francois_perin@hotmail.fr>
2016-02-03 08:18:35 +0100

    Move Contest.Business to Contest.Business.Contest

--- Core/FrameworkExtension/EnumerableExtension.cs
+++ Core/FrameworkExtension/EnumerableExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Contest.Core.FrameworkExtension
 {
@@ -17,6 +18,26 @@ namespace Contest.Core.FrameworkExtension
                 list[k] = list[n];
                 list[n] = value;
             }
+        }
+
+        /// <summary>
+        /// Build a random list with specified list.
+        /// </summary>
+        /// <param name="list">List to randomize</param>
+        /// <returns>Randomized list.</returns>
+        public static IList<T> BuildRandomList<T>(this IList<T> list)
+        {
+            if (list == null) throw new ArgumentNullException("teamList");
+
+            var randomSort = new SortedDictionary<int, T>();
+            var rand = new Random();
+            foreach (var item in list)
+            {
+                int randomPosition;
+                while (randomSort.ContainsKey(randomPosition = rand.Next())) { }
+                randomSort.Add(randomPosition, item);
+            }
+            return randomSort.Values.ToList();
         }
     }
 }
