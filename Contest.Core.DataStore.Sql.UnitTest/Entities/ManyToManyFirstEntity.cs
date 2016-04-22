98f314326a0730b0b313856777c3d38bd75bb681
sGeeK <francois_perin@hotmail.fr>
2016-04-22 17:50:26 +0100

    Setup entity for many to many impl

---
+++ Contest.Core.DataStore.Sql.UnitTest/Entities/ManyToManyFirstEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Contest.Core.DataStore.Sql.Attributes;
+using Contest.Core.DataStore.Sql.ReferenceManyToMany;
+using Contest.Core.Repository;
+
+namespace Contest.Core.DataStore.Sql.UnitTest.Entities
+{
+    [SqlEntity]
+    public class ManyToManyFirstEntity : IIdentifiable, IQueryable
+    {
+        internal static readonly Guid Guid = new Guid("DDA6148D-7316-4C91-827B-10F6A388810E");
+
+        [SqlPrimaryKey]
+        public Guid Id { get; set; }
+
+        [SqlManyToManyReference]
+        public IList<ManyToManySecondEntity> SecondEntityList { get; private set; }
+
+        public static ManyToManyFirstEntity Create()
+        {
+            return new ManyToManyFirstEntity
+            {
+                Id = Guid
+            };
+        }
+
+        public bool AreSame(object other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Contest.Core.DataStore.Sql.UnitTest/Entities/ManyToManySecondEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Contest.Core.DataStore.Sql.Attributes;
+using Contest.Core.DataStore.Sql.ReferenceManyToMany;
+using Contest.Core.Repository;
+
+namespace Contest.Core.DataStore.Sql.UnitTest.Entities
+{
+    [SqlEntity]
+    public class ManyToManySecondEntity : IIdentifiable, IQueryable
+    {
+        public static readonly Guid Guid = new Guid("071318CC-3E81-4243-803A-4B910668BC03");
+
+        [SqlPrimaryKey]
+        public Guid Id { get; set; }
+
+        [SqlManyToManyReference]
+        public IList<ManyToManyFirstEntity> FirstEntityList { get; private set; }
+
+        public static ManyToManySecondEntity Create()
+        {
+            return new ManyToManySecondEntity
+            {
+                Id = Guid
+            };
+        }
+
+        public bool AreSame(object other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
